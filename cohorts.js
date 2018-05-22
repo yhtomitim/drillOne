4da08b8f60d8d11f838cf4eeecdaf6f9964bf85a
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-05-22 08:21:52 -0600

    import data

--- cohorts.js
+++ cohorts.js
@@ -1,1 +1,28 @@
-module.exports = 
+module.exports = {
+  data: [
+    {
+      "ID": 1,
+      "Cohort Name": "17-01-WD-DP",
+      "Cohort Code": "g100",
+      "Number of Students": 28
+    },
+    {
+      "ID": 2,
+      "Cohort Name": "17-01-DS-GT",
+      "Cohort Code": "g105",
+      "Number of Students": 24
+    },
+    {
+      "ID": 3,
+      "Cohort Name": "17-02-WD-PX",
+      "Cohort Code": "g109",
+      "Number of Students": 30
+    },
+    {
+      "ID": 4,
+      "Cohort Name": "17-03-WD-BD",
+      "Cohort Code": "g110",
+      "Number of Students": 29
+    }
+  ]
+} 
